403f924c1cd13afbd29748d90f83e406f68ac030
yashacb <yashwanthbetha@gmail.com>
2016-03-26 01:12:19 +0530

    Updated the crawler code .

--- crawler_app/functions.py
+++ crawler_app/functions.py
@@ -1,5 +1,7 @@
 import re
 from urllib.parse import urljoin
+import re
+from bs4 import BeautifulSoup
 # def csea_exceptions(link) :
 #     if(link.startswith('http://csea.iitg.ernet.in') and link.endswith('-')) :
 #         pattern = re.compile('http://csea.iitg.ernet.in/csea/Public/web_new/index.php/activities/([a-zA-Z-_0-9]+)/.*')
@@ -15,7 +17,10 @@ def is_intranet_link(link) :
     if link == None :
          return False
     else :
-        refined = link[7:]
+        if link.startswith('https://') :
+            refined = link[8:]
+        else :
+            refined = link[7:]
         if refined.startswith('iitg.ernet.in') or refined.startswith('www.iitg.ernet.in') :
             return True
         else :
@@ -24,6 +29,14 @@ def is_intranet_link(link) :
             else :
                 return False
 
+def link_check(link , actual_link) :
+    return actual_link != None and (not actual_link.startswith('http://csea.iitg.ernet.in/csea/Public/web_new/index.php/activities')) \
+    and (not link.startswith('http://intranet.iitg.ernet.in/eventcal/')) \
+    and (not link.startswith('http://shilloi.iitg.ernet.in/~hss/reservation')) and is_intranet_link(actual_link) \
+    and (not actual_link.startswith('http://jatinga.iitg.ernet.in/~dppc/resources/resources/')) and len(actual_link) <= 200 \
+    and (not actual_link.startswith('http://jatinga.iitg.ernet.in/cseforum/calendar.php'))
+
+# Returns false if link appended with ext is the same as link .
 def relativeLinkFilter(link , ext) :
     if link != None and ext != None :
         if not ('#' in ext) :
@@ -32,10 +45,12 @@ def relativeLinkFilter(link , ext) :
             ind = ext.find('#')
             if ind == 0 :
                 return False
-            if not link.endswith(ext[:ind])                 :
-                return True
             else :
-                return False
+                complete = urljoin(link , ext[:ind])
+                if complete != link and complete.count('#') <= 1 :
+                    return True
+                else :
+                    return False
     elif ext == None and link != None :
         return True
     else :
--- crawler_app/views.py
+++ crawler_app/views.py
@@ -10,6 +10,7 @@ import urllib.error
 from urllib.parse import urljoin
 import re
 import time
+import pickle
 
 def index(request) :
     form = SearchForm()
@@ -19,8 +20,11 @@ def index(request) :
 def crawl(request) :
     start_time = time.time()
     default_url = 'http://intranet.iitg.ernet.in' ;
-    visited = {None} ;
-    pdfs = {None} ;
+    visited = {None}
+    text = {None}
+    pdfs = {None}
+    images = {None}
+    others = {None}
     unvisited = {default_url}
     while unvisited :
         link = unvisited.pop()
@@ -32,17 +36,52 @@ def crawl(request) :
                 if 'text/html' in connection_info['Content-Type'] :
                     bsoup = BeautifulSoup(open_connection.read())
                     print(link)
+                    text.add(link[:len(link) if (not '#' in link) else link.find('#')])
+                    # All 'a' tags
                     all_links = bsoup.find_all('a')
                     for li in all_links :
                         ext = li.get('href')
                         if ext != None and ext != '' and (functions.relativeLinkFilter(link , ext)) and (not ext.startswith('mailto:')) :
                             actual_link = urljoin(link , ext)
-                            if actual_link != None and (not actual_link.startswith('http://csea.iitg.ernet.in/csea/Public/web_new/index.php/activities')) \
-                            and (not actual_link in visited) and (not link.startswith('http://intranet.iitg.ernet.in/eventcal/')) \
-                            and (not link.startswith('http://shilloi.iitg.ernet.in/~hss/reservation')) and functions.is_intranet_link(actual_link) \
-                            and (not actual_link.startswith('http://jatinga.iitg.ernet.in/~dppc/resources/resources/')) and len(actual_link) <= 200 \
-                            and (not actual_link.startswith('http://jatinga.iitg.ernet.in/cseforum/calendar.php')) :
+                            if functions.link_check(link , actual_link) and (not actual_link in visited) :
+                                actual_link = urllib.parse.quote(actual_link.strip() , safe="%/:=&?~#+!$,;'@()*[]")
                                 unvisited.add(actual_link)
+                    # End
+                    # All frame tags
+                    frame_links = bsoup.find_all('frame')
+                    for li in frame_links :
+                        ext = li.get('src')
+                        if ext != None and ext != '' and functions.relativeLinkFilter(link , ext) and (not ext.startswith('mailto:')) :
+                            actual_link = urljoin(link , ext)
+                        if functions.link_check(link , actual_link) and (not actual_link in visited) :
+                            actual_link = urllib.parse.quote(actual_link.strip() , safe="%/:=&?~#+!$,;'@()*[]")
+                            unvisited.add(actual_link)
+                    # End
+                    # All iframe tags
+                    frame_links = bsoup.find_all('iframe')
+                    for li in frame_links :
+                        ext = li.get('src')
+                        if ext != None and ext != '' and functions.relativeLinkFilter(link , ext) and (not ext.startswith('mailto:')) :
+                            actual_link = urljoin(link , ext)
+                        if functions.link_check(link , actual_link) and (not actual_link in visited) :
+                            actual_link = urllib.parse.quote(actual_link.strip() , safe="%/:=&?~#+!$,;'@()*[]")
+                            unvisited.add(actual_link)
+                    # End
+                # For documents
+                elif ('application/pdf' in connection_info['Content-Type']) or ('application/msword' in connection_info['Content-Type']) or \
+                ('application/vnd.openxmlformats-officedocument.wordprocessingml.document' in connection_info['Content-Type']) or \
+                ('application/vnd.ms-excel' in connection_info['Content-Type']) or \
+                ('application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' in connection_info['Content-Type']) or \
+                ('application/vnd.ms-powerpoint' in connection_info['Content-Type']) or \
+                ('application/vnd.openxmlformats-officedocument.presentationml.presentation' in connection_info['Content-Type']) and \
+                functions.is_intranet_link(link) :
+                    link = urllib.parse.quote(link.strip() , safe="%/:=&?~#+!$,;'@()*[]")
+                    pdfs.add(link)
+                # End
+                else :
+                    link = urllib.parse.quote(link.strip() , safe="%/:=&?~#+!$,;'@()*[]")
+                    others.add(link)
+
         except timeout :
             print('Timeout : ' + link)
         except urllib.error.URLError as e :
@@ -53,7 +92,25 @@ def crawl(request) :
             print('Some other error : ' + link)
     print(len(visited))
     print(time.time() - start_time)
-    string = 'Time taken : ' + str(time.time() - start_time) + '<br>'
-    for l in visited :
+    string = 'Time taken : ' + str(time.time() - start_time) + '<br>' + 'HTML pages : ' + str(len(text)) + '<br>'
+    for l in text :
         string = string + str(l) + '<br>'
+    string += 'PDF pages : ' + str(len(pdfs)) + '<br>'
+    for l in pdfs :
+        string = string + str(l) + '<br>'
+    string += 'Other links : ' + str(len(others)) + '<br>'
+    for l in others :
+        string = string + str(l) + '<br>'
+    try :
+        html_file = open('/home/yashwanthbetha/html.txt' , 'wb')
+        pdf_file = open('/home/yashwanthbetha/pdf.txt' , 'wb')
+        other_file = open('/home/yashwanthbetha/other.txt' , 'wb')
+        pickle.dump(text , html_file)
+        pickle.dump(pdfs , pdf_file)
+        pickle.dump(others , other_file)
+        html_file.close()
+        pdf_file.close()
+        other_file.close()
+    except :
+        print('Error in file handling .')
     return HttpResponse('Crawled links : <br> '+ str(len(visited)) + '<br>' + string)
